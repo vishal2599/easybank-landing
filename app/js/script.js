1021a319c23ff450e1d90b3d054f50980b140b51
vshl2599@gmail.com <vshl2599@gmail.com>
2021-01-16 13:57:16 +0530

    Finalising hero Section

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function(){
     if( header.classList.contains('open') ){
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        body.classList.remove('noscroll');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     } else { // Open hamburger Menu
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        body.classList.add('noscroll');
+        fadeElems.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+        });
     }
 });
